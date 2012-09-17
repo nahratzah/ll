f0826ebb62ff2e344f8c5717dd6cc90b588089af
ariane <ariane@stack.nl>
2012-09-17 03:43:24 +0200

    Test LL_PREV functions correct.

---
+++ test/prev.c
@@ -0,0 +1,35 @@
+#include "llobj.h"
+#include <assert.h>
+
+#define N 100
+
+struct objlist list = LL_HEAD_INITIALIZER(list);
+struct obj data[N];
+
+int
+main()
+{
+	int i;
+	struct obj *o, *next_o = NULL;
+
+	for (i = 0; i < N; i++)
+		data[i].satelite = i;
+
+	for (i = 0; i < N; i++) {
+		LL_PUSH_BACK(objlist, &list, &data[i]);
+		LL_RELEASE(objlist, &list, &data[i]);
+	}
+
+	for (i = N - 1, o = LL_LAST(objlist, &list);
+	    i > 0 - 1;
+	    i--, o = next_o) {
+		assert(o != NULL);
+		assert(o->satelite == i);
+
+		next_o = LL_PREV(objlist, &list, o);
+
+		LL_RELEASE(objlist, &list, o);
+	}
+
+	return 0;
+}
